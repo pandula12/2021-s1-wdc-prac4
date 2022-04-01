6c3f7b1349ee8bafd04cbcfdf48e2cdb33270f12
pandula12 <pandulagajadeera09@gmail.com>
2022-04-01 22:44:13 +1030

    prac4

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,24 @@ router.get('/last.txt', function(req, res) {
   time_stamp = new Date().toLocaleString();
 });
 
+var counter = 0;
+router.get('/color.html', function (req, res) {
+  var color = "red";
+  counter++;
+  res.send(`
+  <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/stylesheets/style.css">
+        <title>
+            color
+        </title>
+    </head>
+    <body>
+        <h1 class = "${color}">${color}</h1>
+    </body>
+</html>`);
+});
 
 module.exports = router;
